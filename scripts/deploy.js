df7684dbc0b488387775fec0a73edeab143c852f
Alex <lxg1992@gmail.com>
2022-06-05 15:27:28 -0400

    Added whole contract

---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+  //0x943D45f54d18aD879B68846505557453187b2422
+
+  // Call the function.
+  let txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+  console.log("Minted NFT #1")
+
+  txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+  console.log("Minted NFT #2")
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
